2431726f7a587adbafe6febb836c140fdad3c3d6
megamind <echingalo@gmail.com>
2023-05-05 13:17:11 +0300

    refactor: added bases styles on the project root

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React, { Suspense } from "react";
 import i18n from "@dhis2/d2-i18n";
 import FullPageLoader from "./shared/components/Loaders";
 import AppRouter from "./modules/Router";
+import "./main.css";
+import "./common.css";
 
 const MyApp = () => (
   <div>
