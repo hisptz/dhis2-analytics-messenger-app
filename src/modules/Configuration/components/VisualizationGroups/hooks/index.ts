07493b420b3ea57821d11ab42893751dff36817f
megamind <echingalo@gmail.com>
2023-05-06 16:30:48 +0300

    refactor: added configuration data fetching mechanisms

---
+++ src/modules/Configuration/components/VisualizationGroups/hooks/index.ts
@@ -0,0 +1,53 @@
+import { useDataQuery } from "@dhis2/app-runtime";
+import { useEffect } from "react";
+import { VisualizationGroup } from "../../../../../shared/interfaces";
+import { ANALYTICS_GROUPS_DATASTORE_KEY } from "../../../../../shared/constants/dataStore";
+
+// TODO add mechanism for pagination
+const query = {
+  visualizationGroups: {
+    resource: `dataStore/${ANALYTICS_GROUPS_DATASTORE_KEY}`,
+    params: ({ page, pageSize }: any) => ({
+      fields: [
+        "id",
+        "name",
+        "created",
+        "lastUpdated",
+        "createdBy",
+        "visualizations",
+      ],
+      page: page ?? 1,
+      pageSize: pageSize ?? 10,
+    }),
+  },
+};
+
+export function useVisualizationGroups(): any {
+  const { data, loading, error, refetch } = useDataQuery(query);
+
+  const { entries, pager } = data.visualizationGroups ?? {};
+
+  const visualizationGroups: VisualizationGroup[] = (entries ?? []).map(
+    ({ id, name, created, visualizations, lastUpdated, createdBy }: any) => {
+      return {
+        id: id ?? "",
+        name: name ?? "",
+        created: created ?? "N/A",
+        lastUpdated: lastUpdated ?? "N/A",
+        createdBy: createdBy ?? "N/A",
+        visualizations: (visualizations ?? []).map(({ id, name }: any) => ({
+          id: id ?? "",
+          name: name ?? "",
+        })),
+      };
+    }
+  );
+
+  return {
+    loading,
+    error,
+    refetch,
+    pager,
+    visualizationGroups,
+  };
+}
---
+++ src/shared/constants/dataStore.ts
@@ -0,0 +1,3 @@
+export const ANALYTICS_GROUPS_DATASTORE_KEY = "hisptz-analytics-groups";
+export const GATEWAY_DATASTORE_KEY = "hisptz-analytics-gateways";
+export const PUSH_ANALYTICS_DATASTORE_KEY = "hisptz-push-analytics";
