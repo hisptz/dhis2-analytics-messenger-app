296b02b07ddf6dce451b9533ce6e9df80cf2a88b
Davis799 <davistibz@gmail.com>
2023-07-26 11:22:12 +0300

    test: cypress configuration

--- cypress/e2e/Example.feature
+++ cypress/e2e/Example.feature
@@ -3,4 +3,7 @@ Feature: Testing tests setup
 
   Scenario: Testing if login works
     Given an authorized user
+    When navigating to homepage
+    When  click on a tab
     Then I should be able to access the app homepage
+
--- cypress/e2e/Example/example.ts
+++ cypress/e2e/Example/example.ts
@@ -1,9 +1,11 @@
-import {Given, Then} from "@badeball/cypress-cucumber-preprocessor";
-
+import { Given, Then, When } from "@badeball/cypress-cucumber-preprocessor"
 
 Given(/^an authorized user$/, function () {
-    cy.visit("/");
-});
+  cy.visit("/")
+})
+When(/^navigating to homepage$/, () => {})
+When(/^click on a tab$/, () => {})
+
 Then(/^I should be able to access the app homepage$/, function () {
-    cy.getWithDataTest("{Push Analytics-tab}").should("be.visible");
-});
+  cy.get('[data-test="dhis2-uicore-circularloader"]').should("be.visible")
+})
