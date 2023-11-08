56914f05c3e423325f88b724afeeb9629ea53d9b
Gift Nnko <nnkogift@hotmail.com>
2023-11-08 08:11:46 +0300

    chore(d2 config): Name update
    - Updated app name to add saas

--- d2.config.js
+++ d2.config.js
@@ -1,10 +1,10 @@
 const config = {
-    type: "app",
-    title: "Analytics Messenger",
-    name: "dhis2-analytics-messenger",
-    entryPoints: {
-        app: "./src/App.tsx",
-    },
+	type: "app",
+	title: "Analytics Messenger",
+	name: "dhis2-analytics-messenger-saas",
+	entryPoints: {
+		app: "./src/App.tsx",
+	},
 };
 
 module.exports = config;
