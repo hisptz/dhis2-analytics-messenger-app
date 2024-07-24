6393299403c46ef626801e0438c472df9d483ddd
Gift Nnko <nnkogift@gmail.com>
2024-07-24 11:24:45 +0300

    fix: multi select field
    Fixed issues with multi select throwing error when selected is not part of options
    
    Signed-off-by: Gift Nnko <nnkogift@gmail.com>

--- src/shared/components/Fields/RHFMultiSelectField.tsx
+++ src/shared/components/Fields/RHFMultiSelectField.tsx
@@ -1,46 +1,62 @@
-import {Controller} from "react-hook-form";
-import {MultiSelectField, MultiSelectOption} from "@dhis2/ui";
+import { Controller } from "react-hook-form";
+import { MultiSelectField, MultiSelectOption } from "@dhis2/ui";
 import React from "react";
 
-
 export interface RHFMultiSelectFieldProps {
-    validations?: Record<string, any>
-    name: string;
-    label: string;
-    options: any[];
+	validations?: Record<string, unknown>;
+	name: string;
+	label: string;
+	options: { value: string; label: string }[];
 
-    [x: string]: any;
+	[x: string]: unknown;
 }
 
-
-export function RHFMultiSelectField({name, label, options, ...props}: RHFMultiSelectFieldProps) {
-
-
-    return (
-        <Controller
-            render={({field, fieldState}) => {
-                return (
-                    <MultiSelectField
-                        {...props}
-                        filterable
-                        label={label}
-                        onChange={({selected}: { selected: string[] }) => {
-                            field.onChange(selected?.map((sel) => {
-                                return sel;
-                            }));
-                        }}
-                        selected={Array.isArray(field.value) ? field.value ?? [] : []}
-                        error={!!fieldState.error}
-                        validationText={fieldState.error?.message}
-                    >
-                        {
-                            options?.map(({label, value}: any) => (
-                                <MultiSelectOption key={`${label}-${value}`} label={label} value={value}/>))
-                        }
-                    </MultiSelectField>
-                );
-            }} name={name}
-        />
-    );
-
+export function RHFMultiSelectField({
+	name,
+	label,
+	options,
+	...props
+}: RHFMultiSelectFieldProps) {
+	return (
+		<Controller
+			render={({ field, fieldState }) => {
+				return (
+					<MultiSelectField
+						{...props}
+						filterable
+						label={label}
+						onChange={({ selected }: { selected: string[] }) => {
+							field.onChange(
+								selected?.map((sel) => {
+									return sel;
+								}),
+							);
+						}}
+						selected={
+							Array.isArray(field.value)
+								? field.value.filter(
+										(val) =>
+											!!options.find(
+												(option) =>
+													option.value === val,
+											),
+									)
+								: []
+						}
+						error={!!fieldState.error}
+						validationText={fieldState.error?.message}
+					>
+						{options?.map(({ label, value }) => (
+							<MultiSelectOption
+								key={`${label}-${value}`}
+								label={label}
+								value={value}
+							/>
+						))}
+					</MultiSelectField>
+				);
+			}}
+			name={name}
+		/>
+	);
 }
